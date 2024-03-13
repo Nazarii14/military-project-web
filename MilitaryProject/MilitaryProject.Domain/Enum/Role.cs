65b5b4abd118bf63fc08634aaf564025ee763e6b
TTEJlbMEHb <vladyslav.linnik@lnu.edu.ua>
2024-03-13 20:12:12 +0200

    architecture + EF Core

---
+++ MilitaryProject/MilitaryProject.Domain/Enum/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryProject.Domain.Enum
+{
+    public enum Role
+    {
+        Admin,
+        Commander,
+        Soldier,
+        Volunteer,
+        Guest
+    }
+}
---
+++ MilitaryProject/MilitaryProject.Domain/Helpers/HashPasswordHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryProject.Domain.Helpers
+{
+    public class HashPasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            string salt = "salt123";
+            string saltedPassword = password + salt;
+            using (var sha256 = SHA256.Create())
+            {
+                var hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
+                var hash = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                return hash;
+            }
+        }
+    }
+}
