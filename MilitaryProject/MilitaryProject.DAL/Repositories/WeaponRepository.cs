9ed9a84e3c7714d351f5b0d0f43a76c561869362
WeaRD276-01 <oleksandr.zhenchenko@lnu.edu.ua>
2024-03-21 22:47:18 +0200

    Weapon repository added

---
+++ MilitaryProject/MilitaryProject.DAL/Repositories/WeaponRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilitaryProject.DAL.Interface;
+using MilitaryProject.Domain.Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MilitaryProject.DAL.Repositories
+{
+    public class WeaponRepository : BaseRepository<Weapon>
+    {
+        private readonly ApplicationDbContext _db;
+
+        public WeaponRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task Create(Weapon entity)
+        {
+            await _db.Weapons.AddAsync(entity);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(Weapon entity)
+        {
+            _db.Weapons.Remove(entity);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Weapon>> GetAll()
+        {
+            return await _db.Weapons.ToListAsync();
+        }
+
+        public async Task Update(Weapon entity)
+        {
+            _db.Weapons.Update(entity);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
