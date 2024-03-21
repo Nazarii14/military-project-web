94f202c6daa500a22e1b02bc8b09a33ebc212809
Nazarii14 <rosaka1429@gmail.com>
2024-03-21 18:13:56 +0200

    brigade crud added

---
+++ MilitaryProject/MilitaryProject.DAL/Repositories/BrigadeRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MilitaryProject.DAL.Interface;
+using MilitaryProject.Domain.Entity;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MilitaryProject.DAL.Repositories
+{
+    public class BrigadeRepository : BaseRepository<Brigade>
+    {
+        private readonly ApplicationDbContext _db;
+
+        public BrigadeRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task Create(Brigade entity)
+        {
+            await _db.Brigades.AddAsync(entity);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Delete(Brigade entity)
+        {
+            _db.Brigades.Remove(entity);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Brigade>> GetAll()
+        {
+            return await _db.Brigades.ToListAsync();
+        }
+
+        public async Task Update(Brigade entity)
+        {
+            _db.Brigades.Update(entity);
+            await _db.SaveChangesAsync();
+        }
+    }
+}
