6478587c28103e3052237494e7c40f1d8aec2b8a
Nazarii14 <rosaka1429@gmail.com>
2024-03-22 08:10:30 +0200

    mapper installed, minor changes

---
+++ MilitaryProject/UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Threading.Tasks;
+using Moq;
+using NUnit.Framework;
+using MilitaryProject.BLL.Interfaces;
+using MilitaryProject.Domain.Response;
+using MilitaryProject.Domain.ViewModels.User;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
